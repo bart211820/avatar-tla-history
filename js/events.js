2d0338c81d2afdae45df2a4c17200989d36a5780
bart211820 <bartlans76@gmail.com>
2020-06-07 15:10:29 +0200

    Added timeline
    Only fire nation for now

---
+++ js/events.js
@@ -0,0 +1,255 @@
+var fEvents = [
+
+	{
+		"id" : "f1", 
+		"name" : "Avatar Roku is born", 
+		"year" : "82BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f2", 
+		"name" : "Fire Prince Sozin is born", 
+		"year" : "82BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f3", 
+		"name" : "Sozin becomes the new Fire Lord", 
+		"year" : "58BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f4", 
+		"name" : "Start attack on earth kingdom.", 
+		"year" : "40BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f5", 
+		"name" : "Avatar Roku discovers the fire colonies. And fights Sozin.", 
+		"year" : "37BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f6", 
+		"name" : "Avatar Roku dies", 
+		"year" : "12BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f7", 
+		"name" : "War starts", 
+		"year" : "11BG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f8", 
+		"name" : "Sozin uses comet to kill the Air Nomads", 
+		"year" : "0AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f9", 
+		"name" : "Fire Prince Azulon is born.", 
+		"year" : "0AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f10", 
+		"name" : "Battle of Hun Tui", 
+		"year" : "10AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f11", 
+		"name" : "Fire lord Sozin dies peacefully in his sleep and Azulon becomes the new Fire Lord", 
+		"year" : "20AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f12", 
+		"name" : "Fire nations starts attacking the Southern Water tribe", 
+		"year" : "40AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f13", 
+		"name" : "Fire prince Iroh is born", 
+		"year" : "55AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f14", 
+		"name" : "Fire prince Ozai is born", 
+		"year" : "60AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f15", 
+		"name" : "Fire prince Lu Ten is born", 
+		"year" : "77AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f16", 
+		"name" : "Fire prince Zuko is born", 
+		"year" : "83AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f17", 
+		"name" : "Fire princess Azula is born", 
+		"year" : "85AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f18", 
+		"name" : "The proclaimed last southern water bender is killed by commander Yon Rha", 
+		"year" : "94AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f19", 
+		"name" : "Iroh starts the attack on Ba Sing Se and manages to break through the first wall", 
+		"year" : "94AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f20", 
+		"name" : "Lu Ten dies", 
+		"year" : "95AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f21", 
+		"name" : "Fire lord Azulon was poisoned by his daughter-in-law and dies and Ozai becomes the new Fire Lord", 
+		"year" : "95AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f22", 
+		"name" : "Zuko is banned from the fire nation", 
+		"year" : "96AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f23", 
+		"name" : "Admiral Zhao attacks the Northern Water tribe", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f24", 
+		"name" : "Fire Princess Azula conquers Ba Sing Se and becomes the new Earth King", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f25", 
+		"name" : "Attack during the eclipse", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f26", 
+		"name" : "Iroh takes back Ba Sing Se", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f27", 
+		"name" : "Destroy flying thingos", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	},
+	{
+		"id" : "f28", 
+		"name" : "The avatar returns and ends the great war", 
+		"year" : "100AG", 
+		"month" : "?", 
+		"description" : "", 
+		"positionX" : "", 
+		"positionY" : ""
+	}
+];
--- js/main.js
+++ js/main.js
@@ -1,4 +1,107 @@
 $(document).ready(function(){
-	
-	
+	constructTimeLine();
+	loadFireTimeLine();
+
 });
+
+function constructTimeLine() {
+	var timelineHTML = '';
+	for (i = -82; i < 101; i++) {
+		timelineHTML += '<div class="timelineYear" id="timelineYear' + convertIntToAvatarYear(i) + '">';
+		timelineHTML += '<div class="timelineMonth timelineSpring"></div>';
+		timelineHTML += '<div class="timelineMonth timelineSummer"></div>';
+		timelineHTML += '<div class="timelineMonth timelineAutumn"></div>';
+		timelineHTML += '<div class="timelineMonth timelineWinter"></div>';
+		timelineHTML += '</div>';
+	}
+	document.getElementById("timeline").innerHTML = timelineHTML;
+}
+
+function removeEmtyTimeLineSlots() {
+	var monthDivs = document.querySelectorAll(".timelineYear .timelineMonth");
+	for (i = 0; i < monthDivs.length; i++) {
+		if(monthDivs[i].innerHTML == "") {
+			monthDivs[i].remove();
+		}
+	}
+}
+
+function convertIntToAvatarYear(year) {
+	if(year < 0) {
+		return year.toString().replace("-", "") + 'BG';
+	} else {
+		return year + 'AG';
+	}
+}
+
+function loadFireTimeLine() {
+	constructTimeLine();
+	fEvents.forEach((element) => {
+		eventHTML = '<div class="timeLineEvent"><div class="timeLineImage" style="background-image: url(events/' + element.id + '.png)"></div><p>' + element.year +'</p></div>';
+		switch(element.month) {
+			case "spring":
+				document.querySelector('#timelineYear' + element.year + ' .timelineSpring').innerHTML += eventHTML;
+				break;
+			case "summer":
+				document.querySelector('#timelineYear' + element.year + ' .timelineSummer').innerHTML += eventHTML;
+				break;
+			case "autumn":
+				document.querySelector('#timelineYear' + element.year + ' .timelineAutumn').innerHTML += eventHTML;
+				break;
+			case "winter":
+				document.querySelector('#timelineYear' + element.year + ' .timelineWinter').innerHTML += eventHTML;
+				break;
+			default:
+				document.querySelector('#timelineYear' + element.year).innerHTML += eventHTML;
+		}
+	});
+	removeEmtyTimeLineSlots();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadFireEvents(){
+	var timelineHTML = '';
+	for (i = 0; i < 5; i++) {
+		timelineHTML += '<div class="timelineYear">';
+		fEvents.forEach((element) => {
+
+			if(i == element.index) {
+				switch(element.month) {
+					case "spring":
+						timelineHTML += '<div class="timelineMonth timelineSpring"></div>';
+						break;
+					case "summer":
+						timelineHTML += '<div class="timelineMonth timelineSummer"></div>';
+						break;
+					case "autumn":
+						timelineHTML += '<div class="timelineMonth timelineAutumn"></div>';
+						break;
+					case "winter":
+						timelineHTML += '<div class="timelineMonth timelineWinter"></div>';
+						break;
+					default:
+						// F
+				}
+			}
+		})
+		timelineHTML += '</div>';
+	}
+	console.log(timelineHTML);
+	document.getElementById("timeline").innerHTML = timelineHTML;
+}
